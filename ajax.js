ac1600d2044d018f163051f96590c050873fc643
Xxavier Polk <xxavier@xxavierpolk.com>
2025-01-19 23:23:01 -0500

    First fetch success

--- ajax.js
+++ ajax.js
@@ -1,2 +1,16 @@
 ///////// This module is used for fetching data from an API /////////////
+async function fetchGames() {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/version-group/`);
+        const data = await response.json();
+        console.log(data);
+        return data;
+        
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        alert('An error occurred while fetching data.');
+    }
+}
 
+
+fetchGames();
--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 ///////// This module handles the core logic ///////////////
 
+import { fetchGames } from './ajax.js';
+
+
+fetchGames();
